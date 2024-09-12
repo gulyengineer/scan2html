200cd0410d1d18d8b99f1cea45044d1bb1105818
guly-engineer <94598500+guly-engineer@users.noreply.github.com>
2024-09-12 15:33:29 +0100

    Add test framework

---
+++ test/frontend app/tests/check-page-title.spec.js
@@ -0,0 +1,8 @@
+const { test, expect } = require('@playwright/test')
+
+test('I am on the T-Con page', async ({ page }) => {
+    await page.goto('https://t-con.co.uk/');
+
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle('T-Con');
+})
