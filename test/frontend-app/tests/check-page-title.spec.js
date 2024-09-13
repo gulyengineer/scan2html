ba8fd50e2ecc56e744ecbbd82c146015efb97e30
guly-engineer <94598500+guly-engineer@users.noreply.github.com>
2024-09-13 11:48:00 +0100

    test

--- test/frontend-app/tests/check-page-title.spec.js
+++ test/frontend-app/tests/check-page-title.spec.js
@@ -4,5 +4,5 @@ test('I am on the T-Con page', async ({ page }) => {
     await page.goto('https://t-con.co.uk/');
 
     // Expect a title "to contain" a substring.
-    await expect(page).toHaveTitle('T-Con');
+    await expect(page).toHaveTitle(/T-Con/);
 })
